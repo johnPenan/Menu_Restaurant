38ffd4e3aa9326544174b6e13746390f4c4b0ef9
Jean Penan <jeanpenangoumou1@gmail.com>
2018-05-08 01:38:58 +0200

    déclarations des variables.

--- TP_Menu/src/Tp_Menu.java
+++ TP_Menu/src/Tp_Menu.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Tp_Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String choix;
+		char reponse = ' ';
+		Scanner sc = new Scanner(System.in);
 
 	}
 
